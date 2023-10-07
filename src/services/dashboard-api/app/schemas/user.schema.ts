7ec85704094596aa021243a8ca61576d5ce635ad
xansiety <ferando543@outlook.com>
2023-10-07 13:55:16 -0600

    feat: added zod type's

--- src/services/dashboard-api/app/schemas/user.schema.ts
+++ src/services/dashboard-api/app/schemas/user.schema.ts
@@ -1,15 +1,18 @@
-import { Permissions } from './auth.schema';
-export interface AuthenticatedUser {
-  id: string;
-  email: string;
-  name: string;
-  token: string;
-  refreshToken: string;
-  permissions: Permissions;
-}
+import { z } from 'zod';
+import { PermissionsSchema } from './auth.schema';
 
-export interface User extends Pick<AuthenticatedUser, 'email' | 'name'>{
+// ZOD Type
+export const AuthenticatedUserSchema = z.object({
+  id: z.string(),
+  email: z.string().min(6).max(255).email('Invalid email'),
+  name: z.string(),
+  token: z.string(),
+  refreshToken: z.string().optional(),
+  permissions: PermissionsSchema
+});
+
+export type AuthenticatedUser = z.infer<typeof AuthenticatedUserSchema>;
+
+export interface User extends Pick<AuthenticatedUser, 'email' | 'name'> {
   password: string;
 }
-
-// export type User = Omit<AuthenticatedUser, 'id', 'token', 'refreshToken'>;
